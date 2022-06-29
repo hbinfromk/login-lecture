17373137653cf6fdbba51abbe79013cf215fe0fc
hbinfromk <khb723@naver.com>
2022-06-29 15:05:18 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -14,8 +14,20 @@ class UserStorage {
                 newUsers[field] = users[field];
             }
             return newUsers;
-        }, {});
+        },{});
         return newUsers;
+    }
+
+    static getUserInfo(id){
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users); //=> [id,psword,name]
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        },{});
+
+        return userInfo;
     }
 }
 
